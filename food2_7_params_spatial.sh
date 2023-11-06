d8329d18f201cdc73e3587361f5c5038ae47ff1b
Gary Polhill <gary.polhill@hutton.ac.uk>
2023-11-06 15:38:00 +0000

    Second attempt at SLURM experiment

--- food2_7_params_spatial.sh
+++ food2_7_params_spatial.sh
@@ -29,11 +29,11 @@ MDIR="`pwd`"
 XDIR="`pwd`"
 
 export JAVA_HOME="/mnt/apps/java/jdk-17.0.1"
-cd "/mnt/apps/netlogo-6.2.2"
+cd "/mnt/apps/netlogo-6.3.0"
 OUT="$RDIR/$EXPT_ID.out"
 CSV="$RDIR/$EXPT_ID-table.csv"
 HOME="$tmphome" $SHELL <<TMPHOMESH
-srun  "/mnt/apps/netlogo-6.2.2/netlogo-headless-3gc-4Gi.sh" --model "$MDIR/2023-11-06 household_foodretail v 2_7.nlogo" --setup-file "$XDIR/$XML" --experiment "$EXPT_ID" --threads 1 --table "$CSV" > "$OUT" 2>&1
+srun  "/mnt/apps/netlogo-6.3.0/netlogo-headless-3gc-4Gi.sh" --model "$MDIR/2023-11-06 household_foodretail v 2_7.nlogo" --setup-file "$XDIR/$XML" --experiment "$EXPT_ID" --threads 1 --table "$CSV" > "$OUT" 2>&1
 
 TMPHOMESH
 sleep 5 # Paranoia
